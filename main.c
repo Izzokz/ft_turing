d92ae1a9f78b035809f800f0e9e0825b6a34eb5a
Hugo Villatte <huvillat@2E4.42angouleme.fr>
2026-09-02 14:42:06 +0200

    Adding cJSON + script

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <fcntl.h> //open
+#include <unistd.h>
+#include <stdio.h> //printf
+#include "include/cjson/cJSON.h"
 
 typedef struct s_conf
 {
@@ -10,10 +13,21 @@ typedef struct s_conf
     char **finals;
 } t_conf;
 
-int main(void)
-{
-    
+int main(void) {
+    const char *text = "{\"name\":\"Alice\",\"age\":30}";
+
+    cJSON *json = cJSON_Parse(text);
+
+    cJSON *name = cJSON_GetObjectItem(json, "name");
+    cJSON *age  = cJSON_GetObjectItem(json, "age");
+
+    printf("Name: %s\n", name->valuestring);
+    printf("Age: %d\n", age->valueint);
+
+    cJSON_Delete(json);
+    return 0;
 }
+
 
 // name  str
 // alphabet [c, c, c, c]
